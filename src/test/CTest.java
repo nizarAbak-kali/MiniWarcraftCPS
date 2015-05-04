bba4d3667bd94603d1545d3b2327c3a35bd0c4fb
Nizar Abak-kali <nizarabakkali93@gmail.com>
2015-05-05 00:25:36 +0200

    ajout de la spec du projet \\TODO : une relecture ne serait pas de refus

---
+++ src/test/CTest.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by nizar on 04/05/15.
+ */
+public class CTest {
+
+    public void main(String args[]) {
+        System.out.println("test avec contrat");
+    }
+
+}
---
+++ src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by nizar on 04/05/15.
+ */
+public class Test {
+
+    public void main(String args[]) {
+        System.out.println("test sans contrat");
+    }
+}
