7c78e2084daab60fe460eb6ff5aa7ee519fea3c9
Nizar Abak-kali <nizarabakkali93@gmail.com>
2015-05-05 22:29:55 +0200

    creation du service Terrain

---
+++ src/services/TerrainService.java
@@ -0,0 +1,48 @@
+package services;
+
+/**
+ * Created by nizar on 05/05/15.
+ */
+public interface TerrainService {
+    /* Observators */
+    public Integer largeur();
+
+    public Integer hauteur();
+
+    public TerrainType type();
+
+    public Integer orRestant();
+
+    public boolean estLaminee();
+
+
+
+    /* Invariants */
+    // inv: estLaminee(T) =(min) orRestant(T) <= 0
+
+
+    /* Initializers */
+        /*
+        * pre : largeur >= 0
+        * pre : hauteur >= 0
+        * pre : type != null
+        * pre : orRestant > 0
+        * post : largeur(init(l,h,o,t)) = l
+        * post : hauteur(init(l,h,o,t)) = h
+        * post : orRestant(init(l,h,o,t)) = o
+        * post : type(init(l,h,o,t)) = t
+     */
+    public void init(Integer largeur, Integer hauteur, Integer orRestant, TerrainType type);
+
+
+    /* Operators */
+        /*
+        * pre : orRestant() >= retrait(somme)
+        * pre : somme > 0
+        * pre : !estLaminee();
+        * post : orRestant() == orRestant() - somme
+        * */
+    public void retrait(Integer somme);
+
+
+}
---
+++ src/services/TerrainType.java
@@ -0,0 +1,12 @@
+package services;
+
+/**
+ * Created by nizar on 05/05/15.
+ */
+public enum TerrainType {
+    MURAILLE,
+    ROUTE,
+    MINE,
+    HOTELVILLE,
+    NONE
+}
