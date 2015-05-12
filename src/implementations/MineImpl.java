e261556a2fa242b36663f9b81a6459b5897bdd3d
majdBay <majd.bayassi@hotmail.fr>
2015-05-13 00:31:21 +0200

    Update MineImpl.java

--- src/implementations/MineImpl.java
+++ src/implementations/MineImpl.java
@@ -69,6 +69,17 @@ public class MineImpl extends TerrainImpl implements MineService {
         this.abandonCompteur++;
     }
 
+	@Override
+	public boolean getIsAbandoned() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-//tu code si ta pas compris
+	@Override
+	public boolean getIsAccueil() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
 }
