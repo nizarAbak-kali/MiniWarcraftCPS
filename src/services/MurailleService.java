8aecd4a1dd94947bdf4531f957a98204901ae5c6
majdBay <majd.bayassi@hotmail.fr>
2015-05-13 00:33:16 +0200

    Update MurailleService.java

--- src/services/MurailleService.java
+++ src/services/MurailleService.java
@@ -1,5 +1,8 @@
 package services;
 
+import exceptions.PostConditionException;
+import exceptions.PreConditionException;
+
 /**
  * Created by nizar on 06/05/15.
  */
@@ -16,13 +19,13 @@ public interface MurailleService extends TerrainService {
     *  pre init(largeur,hauteur, pointsRes)
     *  require largeur%2=1 ∧ hauteur%2=1 ∧ pointsRes>0
     * */
-    public void init(Integer largeur, Integer hauteur, Integer pointRes);
+    public void init(Integer largeur, Integer hauteur, Integer pointRes) throws PreConditionException, PostConditionException;
 
     /* Operators */
     /*
     *  pre estfrappee(M, force) require ¬estDetruite(M) ∧ f >= 0
     * */
-    public void estfrappee(Integer force);
+    public void estfrappee(Integer force) throws PreConditionException, PostConditionException;
 
     /* Observations */
      /*
